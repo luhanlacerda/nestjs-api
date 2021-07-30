7a4af97632c41dcaae7436629cb83b11a9e42fdd
luhanlacerda <luhanlacerda@gmail.com>
2021-07-30 17:15:03 -0300

    init

---
+++ src/usuario/usuario.controller.ts
@@ -0,0 +1,22 @@
+import { Body, Controller, Get, Param, Post } from "@nestjs/common";
+import { Usuario } from "./usuario.entity";
+import { UsuarioService } from "./usuario.service";
+
+@Controller('users')
+export class UsuarioController {
+
+    constructor(private usuarioService: UsuarioService) {}
+
+    @Post()
+    public cria(@Body() usuario: Usuario) {
+        const usuarioCriado = this.usuarioService.cria(usuario)
+
+        return usuarioCriado
+    }
+
+    @Get(':nomeDeUsuario')
+    public buscaPorNomeDeUsuario(@Param('nomeDeUsuario') nomeDeUsuario: string) {
+        const usuarioEncontrado = this.usuarioService.buscaPorNomeDeUsuario(nomeDeUsuario)
+        return usuarioEncontrado
+    }
+}
---
+++ src/usuario/usuario.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from "@nestjs/common"
+import { Usuario } from "./usuario.entity"
+
+@Injectable()
+export class UsuarioService {
+    
+    private usuarios: Array<Usuario> = [{
+        id: 1,
+        nomeDeUsuario: "luhan",
+        email: 'luhan@gmail.com',
+        senha: '123456',
+        nomeCompleto: "Luhan Lacerda",
+        dataDeEntrada: new Date()
+    }]
+
+    public cria(usuario: Usuario): Usuario {
+        this.usuarios.push(usuario)
+
+        return usuario
+    }
+
+    public buscaPorNomeDeUsuario(nomeDeUsuario: string): Usuario {
+        return this.usuarios.find(usuario => usuario.nomeDeUsuario == nomeDeUsuario)
+    }
+
+}
